96287c06b67f29aa105074e21446ffe85f17dfea
lichen <119449234@qq.com>
2018-09-20 11:35:05 +0800

    solve import只能使用相对路径 startsWith处理数字要先转化字符串

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -1,5 +1,8 @@
 // pages/classic/classic.js
-import { HTTP } from '/util/http.js'
+
+// 在小程序开发工具中，在Sources中可以观察到'/util/http.js'可能是被系统当成了相对路径来执行，说明 import 只接受相对路径的写法，组件可以使用绝对路径
+// import { HTTP } from '/util/http.js'
+import { HTTP } from '../../util/http.js'
 let http = new HTTP()
 Page({
 
@@ -31,27 +34,27 @@ Page({
         //let that = this 和 wx.request({...})是平级关系，所以that可以被wx.request所以引用
         // let that = this;
         // 小程序里的wx.request是强制异步函数，不可以写成 let data = wx.request({...})的同步形式
-        wx.request({
-            // console提示 'http://bl.7yue.pro 不在以下 request 合法域名列表中...'， 需要打勾“不校验合法域名...”
-            // console提示 'GET http://bl.7yue.pro/v1/classic/latest 401 (Unauthorized)'，代表了未授权，可以查看Network
-            // Network下点击latest，在Preview中查看到缺少appkey的错误信息，msg:"The appkey is useless，please go to the website www.7yue.pro to apply for the appkey"
-            // 当不写method，默认方法为get
-            url: 'http://bl.7yue.pro/v1/classic/latest',
-            // 如果不写header，appkey就要写在url中latest后面，以问号?打头
-            // url: 'http://bl.7yue.pro/v1/classic/latest?appkey=MA0OKyXMxkLNEAIz'
-            // 加上appkey后，再次查看Network中的latest，成功获取到了服务器的数据
-            // 之后要对访问请求做封装
-            header: {
-                appkey: "MA0OKyXMxkLNEAIz"
-            },
-            // res会拿到访问url后，它带有格式的数据
-            // success: function(res) {
-            success: (res) => {
-                // console.log(res)
-                console.log(this.data.test)
+        // wx.request({
+        //     // console提示 'http://bl.7yue.pro 不在以下 request 合法域名列表中...'， 需要打勾“不校验合法域名...”
+        //     // console提示 'GET http://bl.7yue.pro/v1/classic/latest 401 (Unauthorized)'，代表了未授权，可以查看Network
+        //     // Network下点击latest，在Preview中查看到缺少appkey的错误信息，msg:"The appkey is useless，please go to the website www.7yue.pro to apply for the appkey"
+        //     // 当不写method，默认方法为get
+        //     url: 'http://bl.7yue.pro/v1/classic/latest',
+        //     // 如果不写header，appkey就要写在url中latest后面，以问号?打头
+        //     // url: 'http://bl.7yue.pro/v1/classic/latest?appkey=MA0OKyXMxkLNEAIz'
+        //     // 加上appkey后，再次查看Network中的latest，成功获取到了服务器的数据
+        //     // 之后要对访问请求做封装
+        //     header: {
+        //         appkey: "MA0OKyXMxkLNEAIz"
+        //     },
+        //     // res会拿到访问url后，它带有格式的数据
+        //     // success: function(res) {
+        //     success: (res) => {
+        //         // console.log(res)
+        //         console.log(this.data.test)
 
-            }
-        })
+        //     }
+        // })
     },
 
     /**
--- util/http.js
+++ util/http.js
@@ -1,7 +1,10 @@
 // import { config } from '/config.js'里的{config}可以写成新名的方式 {config as config1}
 // import { config as config1} from '/config.js'
 // 当有多个外部引用时，可以写成 import {config, fun1, fun2} form '...'的形式
-import { config } from '/config.js'
+// import只接受相对路径，组件可以使用绝对路径
+// import { config } from '/config.js'
+
+import { config } from '../config.js'
 class HTTP {
     // 在现代编程中，很多时候是不区分【函数】和【方法】的叫法，但在类Class下叫 【方法】
     // request(){}是定义出的方法体
@@ -14,11 +17,11 @@ class HTTP {
         }
         wx.request({
             // params.url是api文档中的 GET   /classic/latest
-            url: config.api_base_url + 'params.url',
+            url: config.api_base_url + params.url,
             method: params.method,
             //params.data是什么？？？需要先设置出params.data吗？
             data: params.data,
-            // postman里2种url写法都是可以的：
+            // header中写入appkey的原因可以参考postman里2种url的写法：
             // 第一种，url中直接填入 GET  http://bl.7yue.pro/v1/classic/latest?appkey=MA0OKyXMxkLNEAIz
             // 第二种，url中填入 GET  http://bl.7yue.pro/v1/classic/latest   然后在header中填入对应的key和value，即appkey和MA0OKyXMxkLNEAIz
             header: {
@@ -27,7 +30,10 @@ class HTTP {
             },
             success: (res) => {
                 // 通过statusCode来获取状态码，如2XX，4XX，5XX等
-                let code = res.statusCode
+                let code = res.statusCode.toString()
+                    // statusCode一般是数字类型，而startsWith只能处理字符串，如果不加toString()会报错：code.startsWith is not a function
+                console.log('res.statusCode is ', res.statusCode)
+                console.log('string "code" is ', code)
                 if (code.startsWith('2')) {
 
                 } else {
