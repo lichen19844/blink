bb78972c14ef4afaaa1cd736656e5435ed9ba029
lichen <119449234@qq.com>
2018-10-17 18:02:30 +0800

    start music

---
+++ components/classic/music/index.js
@@ -0,0 +1,25 @@
+// components/classic/music/index.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    img: String,
+    content: String
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    pauseSrc: 'images/player@waitting.png',
+    playSrc: 'images/player@playing.png'
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
