6857ec9d9bb6c8f6f594ce526d5dc49ce0be557f
lichen <119449234@qq.com>
2018-11-02 22:25:39 +0800

    start music

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -19,13 +19,20 @@ Component({
   data: {
     // 把图片的路径变成js变量
     pauseSrc: 'images/player@pause.png',
-    playSrc: 'images/player@play.png'
+    playSrc: 'images/player@play.png',
+    // {{!playing ? playSrc: pauseSrc}}等同于{{playing ? pauseSrc: playSrc}}更直观一点，其中playing是我们定义的“正在播放”，然而我们给playing的初值是false，false、true和播不播放的状态无关，!playing为true显示play图标，人为定义不播放；点击一下，!playing变成false，显示pause图标，人为定义正在播放
+    playing: false
   },
 
   /**
    * Component methods
    */
   methods: {
-
+    onPlay: function(event){
+      // 图片要能切换
+      this.setData({
+        playing: true
+      })
+    }
   }
 })
