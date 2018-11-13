7b4e3aa73a027ac2f5ece1bc23718149477b5e20
lichen <119449234@qq.com>
2018-11-13 16:54:08 +0800

    try animation

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -4,6 +4,12 @@ import {classicBeh} from '../classic-beh.js';
 
 // 定义一个变量，作为音乐管理对象的方法
 const mMgr = wx.getBackgroundAudioManager();
+const _animation = wx.createAnimation({
+  duration: 500,
+  timingFunction: 'linear',
+  delay: 0,
+  transformOrigin: '50% 50% 0'  
+});
 
 Component({
   /**
@@ -26,7 +32,8 @@ Component({
     playSrc: 'images/player@play.png',
     // {{!playing ? playSrc: pauseSrc}}等同于{{playing ? pauseSrc: playSrc}}更直观一点，其中playing是我们人为定义的“正在播放”，然而我们给playing的初值是false，初值false、true和播不播放的状态无关，只和图标有关，当!playing为true显示play图标，人为定义不播放；点击一下，!playing变成false，显示pause图标，人为定义正在播放
     // 视图层的!playing，其中playing的值false和true只是!的开关，出现在视图层的!只关联图片的切换，然后再映射到视图层
-    playing: false
+    playing: false,
+    animationStyle: ''
   },
 
   // 频繁使用wx:if会执行完整的生命周期并初始化，如data里的值会恢复初值；而hidden不会触发生命周期函数，所以detached只对wx:if生效，对hidden无效
@@ -53,9 +60,9 @@ Component({
         this.setData({
           playing: true
         })
-      // 默认为空字符串，不启动音乐，当设置了新的 src 时，会自动开始播放
-      // 根据惯例，暂停图标所关联的音乐要开始播放
-      mMgr.src = this.properties.src        
+        // 默认为空字符串，不启动音乐，当设置了新的 src 时，会自动开始播放
+        // 根据惯例，暂停图标所关联的音乐要开始播放
+        mMgr.src = this.properties.src        
       } 
       // 因为上次点击后playing的值变为true，此时再点击的话，!this.data.playing的结果就会为假，执行else
       else{
@@ -73,8 +80,11 @@ Component({
       // ❤️网上摘抄实验有bug，如果单纯的用:if(mMgr.paused)则会判断无音乐和音乐播放时都为假，这样两种不同的情况执行相同的操作，则会发生意外，所以需要添加这样的判断，if (typeof (mMgr.paused) !== "undefined") 用以区分播放和无音乐事件。
       // if(typeof(mMgr.paused)!== "undefined"){
       if(mMgr.paused){
+          // 播放小图标的动画
+          _animation.rotate(0).scale(1).translate(0, 0).step();
           this.setData({
-          playing: false
+          playing: false,
+          animationStyle: _animation.export()
         })
         console.log('testPlayingPause')
         return
@@ -84,8 +94,11 @@ Component({
       }
       // 判断当前播放的音乐地址就是properties中的音乐地址，mMgr.src是属性，更像一种拥有的状态
       if(mMgr.src == this.properties.src){
+        // 播放小图标的动画
+        _animation.rotate(45).scale(1.2).translate(0, 0).step();
         this.setData({
-          playing: true
+          playing: true,
+          animationStyle: _animation.export()
         })
         console.log('testPlayingPlay')        
       }
