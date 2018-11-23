8d35e18a553e56702b0564362cd85fa9999a614b
lichen <119449234@qq.com>
2018-11-23 22:03:00 +0800

    resolve music play paused

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -71,8 +71,18 @@ Component({
         })
         // 默认为空字符串，不启动音乐，当设置了新的 src 时，会自动开始播放
         // 根据惯例，暂停图标所关联的音乐要开始播放
-        mMgr.src = this.properties.src
-        mMgr.title = this.properties.title         
+        
+        if(!mMgr.src){
+          mMgr.src = this.properties.src
+        }  else {
+          if(mMgr.src !== this.properties.src){
+            mMgr.src = this.properties.src
+          }else {
+            mMgr.play()
+          }
+        }      
+        
+        mMgr.title = this.properties.title
       } 
       // 因为上次点击后playing的值变为true，此时再点击的话，!this.data.playing的结果就会为假，执行else
       else{
