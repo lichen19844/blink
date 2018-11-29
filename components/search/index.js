508cd3a5b0028d54c002620427b7d22a299eda1e
lichen <119449234@qq.com>
2018-11-29 21:06:42 +0800

    history search

--- components/search/index.js
+++ components/search/index.js
@@ -1,4 +1,11 @@
 // components/search/index.js
+
+import {
+  KeywordModel
+} from '../../models/keyword.js'
+
+const keywordModel = new KeywordModel();
+
 Component({
   /**
    * Component properties
@@ -11,15 +18,30 @@ Component({
    * Component initial data
    */
   data: {
+    // 调用Models中的相关方法，获取到历史搜索的所有关键字数据，然后做setData数据绑定
+    historyWords: []
+  },
 
+  attached() {
+    const historyWords = keywordModel.getHistory();
+    this.setData({
+      historyWords: historyWords
+    })
   },
 
   /**
    * Component methods
    */
   methods: {
-    onCancel(event){
+    onCancel(event) {
       this.triggerEvent('cancel', {}, {})
+    },
+
+    onConfirm(event) {
+      const word = event.detail.value;
+      keywordModel.addToHistory(word);  
     }
+
+
   }
 })
--- models/keyword.js
+++ models/keyword.js
@@ -1,4 +1,4 @@
-class KeyWordModel {
+class KeywordModel {
 
     key = 'q'
     maxLength = 10
@@ -24,7 +24,7 @@ class KeyWordModel {
         // 假设缓存中已经存在了一个有效数组，为避免重复添加元素，先判断keyword是否已经存在，只有keyword没有出现过，才添加keyword
         // 1、拿到历史关键字的数组
         let words = this.getHistory();
-        // 2、 判断是否存在 可以用ES5的indexOf 或ES6的 includes方法
+        // 2、 判断输入的信息是否存在 可以用ES5的indexOf 或ES6的 includes方法
         const has = words.includes(keyword);
         // 实现了 队列
         if(!has) {
@@ -34,9 +34,11 @@ class KeyWordModel {
                 words.pop()
             }
             words.unshift(keyword)
-            // value是一组数组
+            // 更新缓存数据，setStorageSync的value是一组数组words
             wx.setStorageSync(this.key, words)            
         }
 
     }
 }
+
+export { KeywordModel }
